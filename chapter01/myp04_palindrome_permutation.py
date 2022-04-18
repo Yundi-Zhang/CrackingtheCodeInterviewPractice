6116a6b208601ab00214b99a845ec1e2933ee44f
Yundi Zhang <yundi.zhang@outlook.com>
2022-04-18 12:03:50 +0800

    finished chp01/04

--- chapter01/myp04_palindrome_permutation.py
+++ chapter01/myp04_palindrome_permutation.py
@@ -1,7 +1,9 @@
 import unittest
 from collections import defaultdict, Counter
 import time
+import string
 
+# O(N) (also the time complexity of the collections.Counter())
 def palindrome_permutation(text: str) -> bool:
     counter = Counter(text.lower())
     single_num = 0
@@ -12,6 +14,32 @@ def palindrome_permutation(text: str) -> bool:
                 if single_num > 1:
                     return False
     return True
+
+# O(N)
+def palindrome_permutation_no_Counter(text: str) -> bool:
+    clean_char = [ c for c in text.lower() if c in string.ascii_lowercase]
+    counter = {}
+    for c in clean_char:
+        if c not in counter:
+            counter[c] = 1
+        else:
+            counter[c] += 1
+    
+    return sum(count%2 for count in counter.values()) <= 1
+
+# O(N)
+def palindrome_permutation_bit_vector(text: str) -> bool:
+    clean_char = [ c for c in text.lower() if c in string.ascii_lowercase]
+    r = 0
+    for c in clean_char:
+        val = ord(c)
+        mask = 1 << val
+        if r & mask:
+            r &= ~mask
+        else:
+            r |= mask
+    
+    return (r-1) & r == 0
 
 class Test_Palindrome_Permutation(unittest.TestCase):
     test_cases = [
@@ -32,11 +60,13 @@ class Test_Palindrome_Permutation(unittest.TestCase):
     ]
 
     test_functions = [
-        palindrome_permutation
+        palindrome_permutation,
+        palindrome_permutation_no_Counter,
+        palindrome_permutation_bit_vector
     ]
 
     def test_palindrome_permutation(self):
-        number_runs = 1
+        number_runs = 1000
         runtimes = defaultdict(float)
         for _ in range(number_runs):
             for case, res in self.test_cases:
